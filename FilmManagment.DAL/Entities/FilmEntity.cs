702493dc466c3a6e46a775af7e97e747fbe39103
Patrik Červený <xcerve27@vutbr.cz>
2020-03-17 20:26:06 +0100

    Added EqualityComparers

--- FilmManagment.DAL/Entities/FilmEntity.cs
+++ FilmManagment.DAL/Entities/FilmEntity.cs
@@ -23,5 +23,68 @@ namespace FilmManagment.DAL.Entities
 
         public List<ActorEntity> Actors { get; set; } = new List<ActorEntity>();
         public List<RatingEntity> Ratings { get; set; } = new List<RatingEntity>();
+
+
+        private sealed class FilmsEqualityComparer : IEqualityComparer<FilmEntity>
+        {
+            public bool Equals(FilmEntity x, FilmEntity y)
+            {
+                if (ReferenceEquals(x, y))
+                {
+                    return true;
+                }
+
+                if (ReferenceEquals(x, null))
+                {
+                    return false;
+                }
+
+                if (ReferenceEquals(y, null))
+                {
+                    return false;
+                }
+
+                if (x.GetType() != y.GetType())
+                {
+                    return false;
+                }
+
+                return x.Id.Equals(y.Id) &&
+                       string.Equals(x.OriginalName, y.OriginalName) &&
+                       string.Equals(x.CzechName, y.CzechName) &&
+                       string.Equals(x.DirectorsName, y.DirectorsName) &&
+                       string.Equals(x.CountryOfOrigin, y.CountryOfOrigin) &&
+                       string.Equals(x.Description, y.Description) &&
+                       string.Equals(x.ImageFilePath, y.ImageFilePath) &&
+                       x.GenreOfFilm == y.GenreOfFilm &&
+                       x.LengthInMinutes.Equals(y.LengthInMinutes) &&
+                       x.AvarageRatingInPercents.Equals(y.AvarageRatingInPercents) &&
+                       x.Actors.OrderBy(actor => actor.Id).SequenceEqual(y.Actors.OrderBy(actor => actor.Id));          // TODO: consultation with teacher
+                       x.Ratings.OrderBy(rating => rating.Id).SequenceEqual(y.Ratings.OrderBy(rating => rating.Id));    // TODO: consultation with teacher
+            }
+
+            public int GetHashCode(FilmEntity obj)
+            {
+                unchecked
+                {
+                    var hashCode = obj.Id.GetHashCode();
+                    hashCode = (hashCode * 397) ^ (obj.OriginalName != null ? obj.OriginalName.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.CzechName != null ? obj.CzechName.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.DirectorsName != null ? obj.DirectorsName.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.CountryOfOrigin != null ? obj.CountryOfOrigin.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.Description != null ? obj.Description.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (obj.ImageFilePath != null ? obj.ImageFilePath.GetHashCode() : 0);
+                    hashCode = (hashCode * 397) ^ (int)obj.GenreOfFilm;
+                    hashCode = (hashCode * 397) ^ obj.LengthInMinutes.GetHashCode();
+                    hashCode = (hashCode * 397) ^ obj.AvarageRatingInPercents.GetHashCode();
+                    return hashCode;
+                }
+            }
+        }
+
+        // Property which is used to approach methods for testing in class ActorEqualityComparer
+        public static IEqualityComparer<FilmEntity> FilmEqualityComparer { get; } = new FilmsEqualityComparer();
     }
 }
+   
+
--- FilmManagment.DAL/Entities/RatingEntity.cs
+++ FilmManagment.DAL/Entities/RatingEntity.cs
@@ -1,12 +1,59 @@
-﻿
+﻿using System.Collections.Generic;
+
 namespace FilmManagment.DAL.Entities
 {
     /// <summary>
     /// This class contains user's rating of one film.
     /// </summary>
-    public class RatingEntity
+    public class RatingEntity : EntityBase
     {
         public int RatingInPercents { get; set; } = 0;
         public string TextRating { get; set; } = string.Empty;
+
+
+        private sealed class RatingsEqualityComparer : IEqualityComparer<RatingEntity>
+        {
+            public bool Equals(RatingEntity x, RatingEntity y)
+            {
+                if (ReferenceEquals(x, y))
+                {
+                    return true;
+                }
+
+                if (ReferenceEquals(x, null))
+                {
+                    return false;
+                }
+
+                if (ReferenceEquals(y, null))
+                {
+                    return false;
+                }
+
+                if (x.GetType() != y.GetType())
+                {
+                    return false;
+                }
+
+                return x.Id.Equals(y.Id) &&
+                       x.RatingInPercents.Equals(y.RatingInPercents) &&
+                       string.Equals(x.TextRating, y.TextRating); 
+                }
+
+            public int GetHashCode(RatingEntity obj)
+            {
+                unchecked
+                {
+                    var hashCode = obj.Id.GetHashCode();
+                    hashCode = (hashCode * 397) ^ obj.RatingInPercents.GetHashCode();
+                    hashCode = (hashCode * 397) ^ (obj.TextRating != null ? obj.TextRating.GetHashCode() : 0);
+                    return hashCode;
+                }
+            }
+        }
+
+        // Property which is used to approach methods for testing in class ActorEqualityComparer
+        public static IEqualityComparer<RatingEntity> RatingEqualityComparer { get; } = new RatingsEqualityComparer();
     }
 }
+
