1fdddb7e35608e28ca6d83a12e93da5a78d90ea8
Matěj Vopálka <xvopal03@vutbr.cz>
2020-03-29 12:52:51 +0200

    Added EqualityComparers to business layer

--- FilmManagment.BL/Models/DetailModels/FilmDetailModel.cs
+++ FilmManagment.BL/Models/DetailModels/FilmDetailModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using FilmManagment.BL.Models.ListModels;
 using FilmManagment.DAL.Enums;
 
@@ -20,7 +21,120 @@ namespace FilmManagment.BL.Models.DetailModels
         public ICollection<FilmActorListModel> Actors { get; set; }
         public ICollection<RatingListModel> Ratings { get; set; }
 
-        // TODO: add EC
+        private sealed class FilmDetailModelEqualityComparer : IEqualityComparer<FilmDetailModel>
+        {
+	        public bool Equals(FilmDetailModel x, FilmDetailModel y)
+	        {
+		        if (ReferenceEquals(x, y)) return true;
+		        if (ReferenceEquals(x, null)) return false;
+		        if (ReferenceEquals(y, null)) return false;
+		        if (x.GetType() != y.GetType()) return false;
+		        return x.Id == y.Id
+		               && x.OriginalName == y.OriginalName
+		               && x.CzechName == y.CzechName
+		               && x.CountryOfOrigin == y.CountryOfOrigin
+		               && x.Description == y.Description
+		               && x.ImageFilePath == y.ImageFilePath
+		               && x.GenreOfFilm == y.GenreOfFilm
+		               && x.LengthInMinutes.Equals(y.LengthInMinutes)
+		               && x.AvarageRatingInPercents.Equals(y.AvarageRatingInPercents)
+		               && x.Directors.OrderBy(i => i.Id).Equals(y.Directors.OrderBy(i => i.Id))
+		               && x.Actors.OrderBy(i => i.Id).SequenceEqual(y.Actors.OrderBy(i => i.Id))
+		               && x.Ratings.OrderBy(i => i.Id).SequenceEqual(y.Ratings.OrderBy(i => i.Id));
+	        }
 
-    }
+	        public int GetHashCode(FilmDetailModel obj)
+	        {
+		        var hashCode = new HashCode();
+		        hashCode.Add(obj.OriginalName);
+		        hashCode.Add(obj.CzechName);
+		        hashCode.Add(obj.CountryOfOrigin);
+		        hashCode.Add(obj.Description);
+		        hashCode.Add(obj.ImageFilePath);
+		        hashCode.Add((int) obj.GenreOfFilm);
+		        hashCode.Add(obj.LengthInMinutes);
+		        hashCode.Add(obj.AvarageRatingInPercents);
+		        hashCode.Add(obj.Directors);
+		        hashCode.Add(obj.Actors);
+		        hashCode.Add(obj.Ratings);
+		        return hashCode.ToHashCode();
+	        }
+        }
+        public static IEqualityComparer<FilmDetailModel> FilmDetailModelComparer { get; } = new FilmDetailModelEqualityComparer();
+        private sealed class FilmDetailModelEqualityComparerWithoutLists : IEqualityComparer<FilmDetailModel>
+        {
+	        public bool Equals(FilmDetailModel x, FilmDetailModel y)
+	        {
+		        if (ReferenceEquals(x, y)) return true;
+		        if (ReferenceEquals(x, null)) return false;
+		        if (ReferenceEquals(y, null)) return false;
+		        if (x.GetType() != y.GetType()) return false;
+		        return x.Id == y.Id
+		               && x.OriginalName == y.OriginalName
+		               && x.CzechName == y.CzechName
+		               && x.CountryOfOrigin == y.CountryOfOrigin
+		               && x.Description == y.Description
+		               && x.ImageFilePath == y.ImageFilePath
+		               && x.GenreOfFilm == y.GenreOfFilm
+		               && x.LengthInMinutes.Equals(y.LengthInMinutes)
+		               && x.AvarageRatingInPercents.Equals(y.AvarageRatingInPercents);
+	        }
+
+	        public int GetHashCode(FilmDetailModel obj)
+	        {
+		        var hashCode = new HashCode();
+		        hashCode.Add(obj.OriginalName);
+		        hashCode.Add(obj.CzechName);
+		        hashCode.Add(obj.CountryOfOrigin);
+		        hashCode.Add(obj.Description);
+		        hashCode.Add(obj.ImageFilePath);
+		        hashCode.Add((int)obj.GenreOfFilm);
+		        hashCode.Add(obj.LengthInMinutes);
+		        hashCode.Add(obj.AvarageRatingInPercents);
+		        return hashCode.ToHashCode();
+	        }
+        }
+
+        public static IEqualityComparer<FilmDetailModel> FilmDetailModelComparerWithoutLists { get; } = new FilmDetailModelEqualityComparerWithoutLists();
+
+        private sealed class FilmDetailModelEqualityComparerWithoutRating : IEqualityComparer<FilmDetailModel>
+        {
+	        public bool Equals(FilmDetailModel x, FilmDetailModel y)
+	        {
+		        if (ReferenceEquals(x, y)) return true;
+		        if (ReferenceEquals(x, null)) return false;
+		        if (ReferenceEquals(y, null)) return false;
+		        if (x.GetType() != y.GetType()) return false;
+		        return x.Id == y.Id
+		               && x.OriginalName == y.OriginalName
+		               && x.CzechName == y.CzechName
+		               && x.CountryOfOrigin == y.CountryOfOrigin
+		               && x.Description == y.Description
+		               && x.ImageFilePath == y.ImageFilePath
+		               && x.GenreOfFilm == y.GenreOfFilm
+		               && x.LengthInMinutes.Equals(y.LengthInMinutes)
+		               && x.AvarageRatingInPercents.Equals(y.AvarageRatingInPercents)
+		               && x.Directors.OrderBy(i => i.Id).SequenceEqual(y.Directors.OrderBy(i => i.Id))
+		               && x.Actors.OrderBy(i => i.Id).SequenceEqual(y.Actors.OrderBy(i => i.Id));
+	        }
+
+	        public int GetHashCode(FilmDetailModel obj)
+	        {
+		        var hashCode = new HashCode();
+		        hashCode.Add(obj.OriginalName);
+		        hashCode.Add(obj.CzechName);
+		        hashCode.Add(obj.CountryOfOrigin);
+		        hashCode.Add(obj.Description);
+		        hashCode.Add(obj.ImageFilePath);
+		        hashCode.Add((int)obj.GenreOfFilm);
+		        hashCode.Add(obj.LengthInMinutes);
+		        hashCode.Add(obj.AvarageRatingInPercents);
+		        hashCode.Add(obj.Directors);
+		        hashCode.Add(obj.Actors);
+		        return hashCode.ToHashCode();
+	        }
+        }
+
+        public static IEqualityComparer<FilmDetailModel> FilmDetailModelComparerWithoutRating { get; } = new FilmDetailModelEqualityComparerWithoutRating();
+	}
 }
--- FilmManagment.BL/Models/ListModels/DirectorListModel.cs
+++ FilmManagment.BL/Models/ListModels/DirectorListModel.cs
@@ -9,7 +9,25 @@ namespace FilmManagment.BL.Models.ListModels
         public string FirstName { get; set; }
         public string SecondName { get; set; }
 
-        // TODO: add EC
+        private sealed class FirstNameSecondNameEqualityComparer : IEqualityComparer<DirectorListModel>
+        {
+	        public bool Equals(DirectorListModel x, DirectorListModel y)
+	        {
+		        if (ReferenceEquals(x, y)) return true;
+		        if (ReferenceEquals(x, null)) return false;
+		        if (ReferenceEquals(y, null)) return false;
+		        if (x.GetType() != y.GetType()) return false;
+		        return x.Id == y.Id
+		               && x.FirstName == y.FirstName
+		               && x.SecondName == y.SecondName;
+	        }
 
+	        public int GetHashCode(DirectorListModel obj)
+	        {
+		        return HashCode.Combine(obj.FirstName, obj.SecondName);
+	        }
+        }
+
+        public static IEqualityComparer<DirectorListModel> FirstNameSecondNameComparer { get; } = new FirstNameSecondNameEqualityComparer();
     }
 }
