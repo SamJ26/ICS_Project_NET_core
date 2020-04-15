aa3e69c8c3fe475baab9c1c94eb104a5d7c04417
Samuel Janek <Samuel Janek>
2020-04-15 10:05:41 +0200

    Added main window design

--- FilmManagment.GUI/MainWindow.xaml.cs
+++ FilmManagment.GUI/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FilmManagment.GUI
-{
+{ 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
