d197bd655e326ed3f2e9b7408e46980db830c549
Samuel Janek <Samuel Janek>
2020-05-04 10:23:22 +0200

    Commit for Matej - bug in ActorMapper

--- FilmManagment.BL/Mappers/ActorMapper.cs
+++ FilmManagment.BL/Mappers/ActorMapper.cs
@@ -35,6 +35,9 @@ namespace FilmManagment.BL.Mappers
 					FilmId = filmActorEntity.Id,
 					ActorId = filmActorEntity.ActorId,
 					ActorName = string.Concat(filmActorEntity.Actor.FirstName, " ", filmActorEntity.Actor.SecondName),
+
+					// TODO: here is a bug - Film is null
+					FilmName = filmActorEntity.Film.OriginalName,
 				}).ToArray()
 			};
 		}
